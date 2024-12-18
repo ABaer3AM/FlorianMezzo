c4973d49c0fd346af4375d3ca48d74d6dc222042
Adam Baer <adam.baer@3aminnovations.com>
2024-12-18 10:38:54 -0500

    Base maui app templete

---
+++ FlorianMezzo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlorianMezzo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
